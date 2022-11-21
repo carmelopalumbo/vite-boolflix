0b420bb892aae4b4fe8998e55c82f069f88a63aa
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-21 19:05:20 +0100

    add-api-calls

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,67 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
+import axios from "axios";
+import { store } from "./data/store.js";
+
 export default {
   name: "App",
   components: {
     AppFooter,
     AppHeader,
   },
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    getApiMovies() {
+      store.moviesData = [];
+      axios
+        .get(store.apiUrlMovies, {
+          params: {
+            api_key: store.api_key,
+            query: store.query,
+          },
+        })
+        .then((result) => {
+          store.moviesData = result.data.results;
+        });
+    },
+    getApiSeries() {
+      store.tvSeriesData = [];
+      axios
+        .get(store.apiUrlSeries, {
+          params: {
+            api_key: store.api_key,
+            query: store.query,
+          },
+        })
+        .then((result) => {
+          store.tvSeriesData = result.data.results;
+        });
+    },
+  },
+
+  mounted() {
+    //this.getApiMovies();
+  },
 };
 </script>
 
 <template>
   <AppHeader />
-  <h1>APP</h1>
+  <div class="container">
+    <div class="row text-white">
+      <ul>
+        <li v-for="(movie, index) in store.moviesData" :key="index">
+          {{ movie.title }}
+        </li>
+      </ul>
+    </div>
+  </div>
   <AppFooter />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
   <header>
     <div class="container">
       <div class="row d-flex align-items-center pt-5">
-        <div class="col">
+        <div class="col-7">
           <img src="/logo-boolflix.png" alt="logo" />
         </div>
         <div class="col-3">
@@ -21,6 +21,9 @@ export default {
               aria-describedby="addon-wrapping"
             />
           </div>
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn">SEARCH</button>
         </div>
       </div>
     </div>
@@ -37,6 +40,13 @@ header {
     border-color: $primary-color;
     box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
       0 0 8px rgba(255, 0, 0, 0.6);
+  }
+  button {
+    background-color: $primary-color;
+    color: white;
+    &:hover {
+      background-color: lighten($primary-color, 10%);
+    }
   }
 }
 </style>
