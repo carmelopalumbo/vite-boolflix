4d1f4a21c56a06a07e67565adc879547ff5dbe87
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-23 09:30:20 +0100

    add-loader-component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "AppLoader",
+};
+</script>
+
+<template>
+  <span class="loader"></span>
+  <h4 class="py-4">CERCANDO TRA UNIVERSI CINEMATOGRAFICI . . .</h4>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/var" as *;
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 3px solid #fff;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+.loader::after {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-bottom-color: $primary-color;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+h4 {
+  color: $primary-color;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,12 @@
 <script>
 import AppCard from "./AppCard.vue";
+import AppLoader from "./AppLoader.vue";
 import { store } from "../data/store.js";
 export default {
   name: "AppMain",
   components: {
     AppCard,
+    AppLoader,
   },
   data() {
     return {
@@ -63,8 +65,7 @@ export default {
 
   <!-- loading -->
   <div class="load-container text-center py-5" v-else>
-    <span class="loader"></span>
-    <h4 class="py-4">CERCANDO TRA UNIVERSI CINEMATOGRAFICI . . .</h4>
+    <AppLoader />
   </div>
 </template>
 
@@ -74,45 +75,6 @@ main {
   h2 {
     color: $primary-color;
     -webkit-text-stroke: 0.5px white;
-  }
-}
-
-.load-container {
-  .loader {
-    width: 48px;
-    height: 48px;
-    border: 3px solid #fff;
-    border-radius: 50%;
-    display: inline-block;
-    position: relative;
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-  }
-  .loader::after {
-    content: "";
-    box-sizing: border-box;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: 56px;
-    height: 56px;
-    border-radius: 50%;
-    border: 3px solid transparent;
-    border-bottom-color: $primary-color;
-  }
-
-  @keyframes rotation {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-
-  h4 {
-    color: $primary-color;
   }
 }
 </style>
